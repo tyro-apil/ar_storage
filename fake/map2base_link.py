fdecbd2c5eaf7cda19038f3acb4240182c86a980
apil <078bct017.apil@pcampus.edu.np>
2024-06-21 19:56:28 +0545

    fix: map2base transform as Odometry msg

--- fake/map2base_link.py
+++ fake/map2base_link.py
@@ -1,28 +1,28 @@
 import rclpy
-from geometry_msgs.msg import TransformStamped
+from nav_msgs.msg import Odometry
 from rclpy.node import Node
 
 
 class TransformPublisher(Node):
   def __init__(self):
     super().__init__("transform_publisher")
-    self.publisher_ = self.create_publisher(TransformStamped, "tf", 10)
+    self.publisher_ = self.create_publisher(Odometry, "/odometry/filtered", 10)
     self.timer_ = self.create_timer(1.0, self.publish_transform)
 
   def publish_transform(self):
-    transform = TransformStamped()
-    transform.header.stamp = self.get_clock().now().to_msg()
-    transform.header.frame_id = "map"
-    transform.child_frame_id = "base_link"
-    transform.transform.translation.x = 0.0  # Set the translation values here
-    transform.transform.translation.y = 0.0
-    transform.transform.translation.z = 0.0
-    transform.transform.rotation.x = 0.0  # Set the rotation values here
-    transform.transform.rotation.y = 0.0
-    transform.transform.rotation.z = 0.0
-    transform.transform.rotation.w = 1.0
+    odometry = Odometry()
+    odometry.header.stamp = self.get_clock().now().to_msg()
+    odometry.header.frame_id = "map"
+    odometry.child_frame_id = "base_link"
+    odometry.pose.pose.position.x = 0.0  # Set the translation values here
+    odometry.pose.pose.position.y = 0.0
+    odometry.pose.pose.position.z = 0.0
+    odometry.pose.pose.orientation.x = 0.0  # Set the rotation values here
+    odometry.pose.pose.orientation.y = 0.0
+    odometry.pose.pose.orientation.z = 0.0
+    odometry.pose.pose.orientation.w = 1.0
 
-    self.publisher_.publish(transform)
+    self.publisher_.publish(odometry)
 
 
 def main(args=None):
