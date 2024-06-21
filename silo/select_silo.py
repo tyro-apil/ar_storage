ac7203941501019e36b4b339529a937229a217e2
apil <078bct017.apil@pcampus.edu.np>
2024-06-21 12:45:08 +0545

    feat: add silo selection node for optimal silo numbers based on silo state w.r.t. map

---
+++ silo/select_silo.py
@@ -0,0 +1,155 @@
+from typing import List
+
+import rclpy
+from rclpy.node import Node
+from silo_msgs.msg import Silo, SiloArray
+from std_msgs.msg import Int8MultiArray
+
+"""
+Priority List:
+1. Team | Opponent or Opponent | Team
+2. Team | Team
+3. Opponent | Opponent
+4. Empty
+5. Team
+6. Opponent
+"""
+
+
+class SiloSelection(Node):
+  def __init__(self):
+    node_name = "silo_selection"
+    super().__init__("silo_selection")
+
+    # Declare team color as parameter
+    self.declare_parameter("team_color", "blue")
+
+    # Timer to publish two best silos
+    self.create_timer(0.05, self.timer_callback)
+    # Subscribe state of silos w.r.t. map
+    self.state_subscriber = self.create_subscription(
+      SiloArray, "state_map", self.state_received_callback, 10
+    )
+    self.state_subscriber
+    # Publisher of list of 2 best optimal silos
+    self.best_silos_publisher = self.create_publisher(
+      Int8MultiArray, "silo_numbers", 10
+    )
+
+    # Get team color as object variable for silo selection
+    self.team_color = (
+      self.get_parameter("team_color").get_parameter_value().string_value
+    )
+    # Set character representations
+    self.TEAM_REPR = "B"
+    self.OPPONENT_REPR = "R"
+    if self.team_color == "red":
+      self.TEAM_REPR, self.OPPONENT_REPR = self.OPPONENT_REPR, self.TEAM_REPR
+
+    # Initialize optimal silos as zero index
+    self.optimal_silos: List[int] = [-1] * 2
+    self.silo_numbers_msg = Int8MultiArray()
+    self.received_msg = None
+
+    # Initialize list of lists for priority
+    self.priority_list = [[] for _ in range(6)]
+    self.get_logger().info(f"{node_name} node started")
+
+    # Boolean list to indicate silos state
+    self.full_silos_index = []
+
+  def timer_callback(self):
+    self.best_silos_publisher.publish(self.silo_numbers_msg)
+
+  def state_received_callback(self, state_msg: SiloArray):
+    self.received_msg = state_msg
+
+    ## Assign priority to each silo
+    # Put index in respective index of priority_list
+    self.set_priority_list(state_msg.silos)
+
+    ## Iterate through priority list
+    # Select nearest silo index in first non-empty list inaside priority list
+    self.update_target()
+    pass
+
+  def set_priority_list(self, silo_array):
+    # reinitalize priority list
+    self.priority_list = [[] for _ in range(6)]
+    # reinitalize full silos index
+    self.full_silos_index = []
+    # update priority list
+    for silo in silo_array:
+      if (
+        silo.state == self.TEAM_REPR + self.OPPONENT_REPR
+        or silo.state == self.OPPONENT_REPR + self.TEAM_REPR
+      ):
+        self.priority_list[0].append(silo.index)
+      elif silo.state == self.TEAM_REPR * 2:
+        self.priority_list[1].append(silo.index)
+      elif silo.state == self.OPPONENT_REPR * 2:
+        self.priority_list[2].append(silo.index)
+      elif silo.state == "":
+        self.priority_list[3].append(silo.index)
+      elif silo.state == self.TEAM_REPR:
+        self.priority_list[4].append(silo.index)
+      elif silo.state == self.OPPONENT_REPR:
+        self.priority_list[5].append(silo.index)
+      else:
+        self.full_silos_index.append(silo.index)
+
+  def update_target(self):
+    second_priority_list = self.priority_list
+    self.optimal_silos = [-1] * 2
+
+    ## Get best option from first priority list
+    for i, silos in enumerate(self.priority_list):
+      if len(silos) == 0:
+        continue
+      optimal_silo_index = self.get_optimal_silo_index(silos)
+      self.optimal_silos[0] = optimal_silo_index
+      second_priority_list[i].remove(optimal_silo_index)
+      break
+
+    ## Get best option from second priority list
+    for i, silos in enumerate(second_priority_list):
+      if len(silos) == 0:
+        continue
+      optimal_silo_index = self.get_optimal_silo_index(silos)
+      self.optimal_silos[1] = optimal_silo_index
+      break
+
+    ## Update silo_numbers_msg
+    self.silo_numbers_msg.data = self.optimal_silos
+
+  def get_optimal_silo_index(self, silo_indexes):
+    optimal_silo_index = -1
+
+    ## Iterate over silo_indexes
+    ## Select silo_index with minimum distance w.r.t. baselink
+    silos_distance: List[float] = [
+      self.get_distance(silo_index) for silo_index in silo_indexes
+    ]
+    optimal_silo_index = silo_indexes[silos_distance.index(min(silos_distance))]
+    return optimal_silo_index
+
+  def get_distane(self, silo_index):
+    return self.received_msg.silos[silo_index].distance
+
+
+def main(args=None):
+  rclpy.init(args=args)
+
+  state_estimation_node = SiloSelection()
+
+  rclpy.spin(state_estimation_node)
+
+  # Destroy the node explicitly
+  # (optional - otherwise it will be done automatically
+  # when the garbage collector destroys the node object)
+  state_estimation_node.destroy_node()
+  rclpy.shutdown()
+
+
+if __name__ == "__main__":
+  main()
