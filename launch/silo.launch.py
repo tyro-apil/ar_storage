f1302ac39264f31f1986264ba8a3732d60f3b24e
apil <078bct017.apil@pcampus.edu.np>
2024-06-25 10:36:25 +0545

    use silo_picam model for inference

--- launch/silo.launch.py
+++ launch/silo.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
   team_color = "blue"
   input_image_topic = "image_raw"
   tracking_topic = "yolo/tracking"
-  model = "o_blunder.pt"
+  model = "silo_picam.pt"
   tracker = "custom_tracker.yaml"
   baselink_pose_topic = "/odometry/filtered"
 
