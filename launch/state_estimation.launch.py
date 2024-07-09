d83c3de8a6da191fedc4d77a426210fed2a32049
apil <078bct017.apil@pcampus.edu.np>
2024-07-09 20:17:36 +0545

    fix: camera_info.yaml path

--- launch/state_estimation.launch.py
+++ launch/state_estimation.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
     get_package_share_directory("oakd"), "config", "common.yaml"
   )
   camera_info_config = os.path.join(
-    get_package_share_directory("oakd"), "config", "camera_info.yaml"
+    get_package_share_directory("silo"), "config", "camera_info.yaml"
   )
 
   namespace = LaunchConfiguration("namespace")
