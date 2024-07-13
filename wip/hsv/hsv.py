66bf3762e4193ac8f09deb20e92d1c94f87618f1
apil <078bct017.apil@pcampus.edu.np>
2024-07-13 15:20:40 +0545

    feat: add mouse_callback to get HSV in sidebyside view

--- wip/hsv/hsv.py
+++ wip/hsv/hsv.py
@@ -14,6 +14,9 @@ from ultralytics.utils.plotting import (
 MODELS_DIR = "/home/apil/main_ws/src/robot/models"
 MODEL_NAME = "picam_mount.pt"
 
+IMG_DIR = "/home/apil/stuff/silo_pics/valid_test"
+RESULTS_DIR = "/home/apil/stuff/silo_pics/debug"
+
 
 def get_match_percent(hsv_img: cv2.Mat, roi: Tuple, mask: cv2.Mat) -> float:
   hsv_img_copy = hsv_img.copy()
@@ -34,52 +37,84 @@ def main():
   model_path = os.path.join(MODELS_DIR, MODEL_NAME)
   model = YOLO(model_path)
 
-  img_path = os.path.join("silo.jpg")
-  img = cv2.imread(img_path)
-  hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+  red1_h_low = 0
+  red1_h_high = 10
+  red1_s_low = 120
+  red1_s_high = 255
+  red1_v_low = 50
+  red1_v_high = 235
 
-  red_h_low = 0
-  red_h_high = 10
-  red_s_low = 100
-  red_s_high = 255
-  red_v_low = 80
-  red_v_high = 255
+  red2_h_low = 170
+  red2_h_high = 180
+  red2_s_low = 120
+  red2_s_high = 255
+  red2_v_low = 60
+  red2_v_high = 220
 
-  blue_h_low = 85
-  blue_h_high = 110
-  blue_s_low = 50
-  blue_s_high = 255
-  blue_v_low = 30
-  blue_v_high = 255
+  blue1_h_low = 80
+  blue1_h_high = 110
+  blue1_s_low = 130
+  blue1_s_high = 170
+  blue1_v_low = 30
+  blue1_v_high = 90
 
-  threshold = 0.30
+  blue2_h_low = 100
+  blue2_h_high = 115
+  blue2_s_low = 100
+  blue2_s_high = 150
+  blue2_v_low = 110
+  blue2_v_high = 230
 
-  red_mask = cv2.inRange(
-    hsv_img, (red_h_low, red_s_low, red_v_low), (red_h_high, red_s_high, red_v_high)
-  )
-  blue_mask = cv2.inRange(
-    hsv_img,
-    (blue_h_low, blue_s_low, blue_v_low),
-    (blue_h_high, blue_s_high, blue_v_high),
-  )
-
-  kernel = np.ones((5, 5), np.uint8)
-
-  red_mask = cv2.dilate(red_mask, kernel, iterations=3)
-  blue_mask = cv2.dilate(blue_mask, kernel, iterations=3)
-
-  combined_mask = cv2.bitwise_or(red_mask, blue_mask)
-  colored_mask = cv2.bitwise_and(img, img, mask=combined_mask)
+  threshold = 0.33
 
   # cv2.imshow("combined_mask", combined_mask)
   # cv2.imshow("colored_mask", colored_mask)
   # cv2.waitKey(0)
 
-  results: List[Results] = model.predict(source=img, imgsz=(512, 928), device=0)
+  results: List[Results] = model.predict(source=IMG_DIR, imgsz=(512, 928), device=0)
 
   for r in results:
+    # breakpoint()
+    file_name = os.path.basename(r.path)
     r = r.cpu().numpy()
-    annotator = Annotator(r.orig_img)
+
+    img: cv2.Mat = r.orig_img
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    red_mask_1 = cv2.inRange(
+      hsv_img,
+      (red1_h_low, red1_s_low, red1_v_low),
+      (red1_h_high, red1_s_high, red1_v_high),
+    )
+    red_mask_2 = cv2.inRange(
+      hsv_img,
+      (red2_h_low, red2_s_low, red2_v_low),
+      (red2_h_high, red2_s_high, red2_v_high),
+    )
+
+    blue_mask_1 = cv2.inRange(
+      hsv_img,
+      (blue1_h_low, blue1_s_low, blue1_v_low),
+      (blue1_h_high, blue1_s_high, blue1_v_high),
+    )
+    blue_mask_2 = cv2.inRange(
+      hsv_img,
+      (blue2_h_low, blue2_s_low, blue2_v_low),
+      (blue2_h_high, blue2_s_high, blue2_v_high),
+    )
+
+    red_mask = cv2.bitwise_or(red_mask_1, red_mask_2)
+    blue_mask = cv2.bitwise_or(blue_mask_1, blue_mask_2)
+
+    kernel = np.ones((5, 5), np.uint8)
+
+    red_mask = cv2.dilate(red_mask, kernel, iterations=2)
+    blue_mask = cv2.dilate(blue_mask, kernel, iterations=2)
+
+    combined_mask = cv2.bitwise_or(red_mask, blue_mask)
+    colored_mask = cv2.bitwise_and(img, img, mask=combined_mask)
+
+    # breakpoint()
 
     boxes: Boxes = r.boxes.cpu().numpy()
 
@@ -151,13 +186,36 @@ def main():
       silo = {"bbox": b, "state": state}
       silos.append(silo)
 
-      annotator.box_label(b, model.names[int(c)])
+      # annotator.box_label(b, model.names[int(c)])
 
-    print(silos)
-    img = annotator.result()
-    cv2.imshow("colored_mask", colored_mask)
-    cv2.imshow("YOLO V8 Detection", img)
-    cv2.waitKey(0)
+    # print(silos)
+    # img = annotator.result()
+    # cv2.imshow("colored_mask", colored_mask)
+    # # cv2.imshow("YOLO V8 Detection", img)
+    bgr_resize = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+    mask_resize = cv2.resize(
+      colored_mask, (colored_mask.shape[1] // 2, colored_mask.shape[0] // 2)
+    )
+    combined_img = cv2.hconcat([bgr_resize, mask_resize])
+    hsv_resize = cv2.resize(hsv_img, (hsv_img.shape[1] // 2, hsv_img.shape[0] // 2))
+
+    def print_hsv_value(event, x, y, flags, param):
+      if event == cv2.EVENT_LBUTTONDOWN:
+        if x >= hsv_resize.shape[1] or y >= hsv_resize.shape[0]:
+          hsv_value = hsv_resize[y, x - hsv_resize.shape[1]]
+        else:
+          hsv_value = hsv_resize[y, x]
+        print(f"HSV value at ({x}, {y}): {hsv_value}")
+
+    cv2.namedWindow("combined_img")
+    cv2.setMouseCallback("combined_img", print_hsv_value)
+
+    cv2.imshow("combined_img", combined_img)
+    if cv2.waitKey(0) == ord("q"):
+      break
+
+    # debug_path = os.path.join(RESULTS_DIR, file_name)
+    # cv2.imwrite(debug_path, colored_mask)
 
   cv2.destroyAllWindows()
   pass
